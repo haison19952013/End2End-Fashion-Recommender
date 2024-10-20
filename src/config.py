054ef4d0c0a884e279cbe3d55e188d6d8facc810
haison19952013 <haison19952013@gmail.com>
2024-10-20 15:23:35 +0700

    add: sepecify embedded data

--- src/config.py
+++ src/config.py
@@ -5,7 +5,10 @@ class Config():
                     'raw_image_path': 'data/fashion_images_v0',
                     'num_negative_samples': 5,
                     'metatrain_path': 'data/meta_train.csv',
-                    'metatest_path': 'data/meta_test.csv'}
+                    'metatest_path': 'data/meta_test.csv',
+                    'scene_embed_path': 'data/scene_embed.json',
+                    'product_embed_path': 'data/product_embed.json'
+                    }
         self.train = {'learning_rate': 0.0001618, # Optimal:  0.0001618
                       'regularization': 0.2076, # Optimal:  0.2076
                       'embedding_dim':64, # Optimal:  64
