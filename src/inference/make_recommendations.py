976a67a1abe808ee9bc9990bc04d29f09727e69d
haison19952013 <haison19952013@gmail.com>
2024-10-21 00:55:04 +0700

    fix: change to scene_path for clearer

--- src/inference/make_recommendations.py
+++ src/inference/make_recommendations.py
@@ -34,7 +34,7 @@ config_ = config.Config()
 # Define command-line arguments using argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Given embedding files makes recommendations")
-    parser.add_argument('--scene_image',default = True ,type=str, help='Image file for scene')
+    parser.add_argument('--scene_path',default = True ,type=str, help='Image file for scene')
     parser.add_argument('--product_embed',default = config_.data['product_embed_path'] ,type=str, help='Product embedding JSON file')
     parser.add_argument('--top_k', type=int, default=10, help='Number of top-scoring products to return per scene')
     parser.add_argument('--output_dir', type=str, default='./output', help='Location to write output HTML files')
@@ -44,13 +44,14 @@ def parse_args():
 def main():
     # Parse command-line arguments
     args = parse_args()
+    assert os.path.exists(args.scene_path), "Scene image file does not exist"
     
     # load the model from the Model Registry and score
     model_name = config_.train['model_name']
     model_uri = f"models:/{model_name}@{'champion'}"
     loaded_model = mlflow.tensorflow.load_model(model_uri)
     get_scene_embed = loaded_model.get_scene_embed
-    unique_scenes = np.array([args.scene_image])
+    unique_scenes = np.array([args.scene_path])
     scene_dict = my_utils.generate_embeddings(unique_scenes, get_scene_embed, 16, "scene")
 
     # Load product and scene embeddings from JSON files
@@ -67,14 +68,13 @@ def main():
     product_embeddings = np.stack(product_embeddings, axis=0)
 
     # Iterate over scenes and find top-k products for each
-    for index, (scene_key, scene_vec) in enumerate(scene_dict.items()):
+    for index, (scene_path, scene_vec) in enumerate(scene_dict.items()):
         scene_embed = np.expand_dims(np.array(scene_vec), axis=0)
         scores_and_indices = my_utils.find_top_k(scene_embed, product_embeddings, args.top_k)
         
         # Save results as HTML
-        filename = os.path.join(args.output_dir, f"{scene_key.split('/')[-1]}.html")
-        my_utils.save_results(filename, scene_key, scores_and_indices, index_to_key)
+        filename = os.path.join(args.output_dir, f"{scene_path.split('/')[-1]}.html")
+        my_utils.save_recommendation_to_html(filename, scene_path, scores_and_indices, index_to_key)
         
-
 if __name__ == "__main__":
     main()
