d0aa965e000853449241acbd18c3d877ac777fa1
haison19952013 <haison19952013@gmail.com>
2024-10-20 15:25:26 +0700

    fix: use function instead

--- src/utils/set_seed.py
+++ src/utils/set_seed.py
@@ -4,15 +4,19 @@ import numpy as np
 import tensorflow as tf
 import config
 
-config_ = config.Config()
-# Set seed for built-in random module
-random.seed(config_.seed)
+def apply():
+    config_ = config.Config()
+    # Set seed for built-in random module
+    random.seed(config_.seed)
 
-# Set seed for NumPy
-np.random.seed(config_.seed)
+    # Set seed for NumPy
+    np.random.seed(config_.seed)
 
-# Set seed for TensorFlow
-tf.random.set_seed(config_.seed)
+    # Set seed for TensorFlow
+    tf.random.set_seed(config_.seed)
 
-# Set PYTHONHASHSEED for reproducibility
-os.environ['PYTHONHASHSEED'] = str(config_.seed)
+    # Set PYTHONHASHSEED for reproducibility
+    os.environ['PYTHONHASHSEED'] = str(config_.seed)
+
+if __name__ == '__main__':
+    apply()
