5da99ce7fe8b91d3f6f72783fb5bb2b0f3828515
haison19952013 <haison19952013@gmail.com>
2024-10-20 15:26:56 +0700

    fix: rename

---
+++ src/post_processing/export_embeddings.py
@@ -0,0 +1,39 @@
+
+"""
+  Generates embedding files given a model and a catalog.
+"""
+from src import config
+import numpy as np
+from src.utils import my_utils  # Utility for getting the valid scene-product pairs
+import mlflow.tensorflow
+
+def main():
+  config_ = config.Config()
+
+  # load the model from the Model Registry and score
+  model_name = config_.train['model_name']
+  model_uri = f"models:/{model_name}@{'champion'}"
+  loaded_model = mlflow.tensorflow.load_model(model_uri)
+  print("--")
+  
+  get_scene_embed = loaded_model.get_scene_embed
+  get_product_embed = loaded_model.get_product_embed
+
+  scene_product = my_utils.get_valid_scene_product(config_.data['raw_image_path'], config_.data['metadata_path'])
+  print(f"Found {len(scene_product)} valid scene product pairs.")
+  # Extract unique scenes and products
+  unique_scenes = np.array(list(set(x[0] for x in scene_product)))
+  unique_products = np.array(list(set(x[1] for x in scene_product)))
+  print(f"Found {len(unique_scenes)} unique scenes.")
+  print(f"Found {len(unique_products)} unique products.")
+
+  # # Process scenes and generate embeddings
+  scene_dict = my_utils.generate_embeddings(unique_scenes, get_scene_embed, 16, "scene")
+  my_utils.save_embeddings(scene_dict, config_.data['scene_embed_path'])
+
+  # # #  Process products and generate embeddings
+  product_dict = my_utils.generate_embeddings(unique_products, get_product_embed, 16, "product")
+  my_utils.save_embeddings(product_dict, config_.data['product_embed_path'])
+
+if __name__ == '__main__':
+  main()
